7a07ad1655dd518b9a28361524224e5c958fdddd
TateKennington <TateKennington@users.noreply.github.com>
2019-07-22 14:11:28 +1200

    Added basic game start and game over

--- CoinDash/Main.gd
+++ CoinDash/Main.gd
@@ -9,37 +9,43 @@ func startGame():
 	score = 0;
 	timeleft = 30;
 	$HUD.updateScore(score)
-	$HUD.updateTimer(timeleft);
-	$GameplayTimer.start();
-	spawnCoin()
+	$HUD.updateTimer(timeleft)
+	$Player.start()
+	$GameplayTimer.start()
+	spawnCoins(1)
 	pass
 
 func gameOver():
 	$GameplayTimer.stop()
 	$Player.die()
+	for coin in $Coins.get_children():
+		coin.queue_free()
+	$HUD/StartButton.show()
+	$HUD/StartButton.disabled = false
 	pass
 
 func _on_GameplayTimer_timeout():
 	timeleft-=1
 	$HUD.updateTimer(timeleft)
-	#TODO: GameOver
+	if timeleft == 0:
+		gameOver()
 	pass
 
 
 func _on_Player_pickup():
 	score+=1
-	timeleft+=10
+	timeleft+=2
 	$HUD.updateScore(score)	
 	$HUD.updateTimer(timeleft)
-	spawnCoin()
+	spawnCoins(1)
 	pass
 
-func spawnCoin():
-	var c = coinScene.instance()
-	var screenSize = get_viewport().get_visible_rect().size;
-	$Coins.add_child(c)
-	c.position = Vector2(rand_range(0, screenSize.x), rand_range(0, screenSize.y))
-	while $Player.position.distance_to(c.position) < 300:
+func spawnCoins(num):
+	for i in range(num):
+		var c = coinScene.instance()
+		var screenSize = get_viewport().get_visible_rect().size;
+		$Coins.add_child(c)
 		c.position = Vector2(rand_range(0, screenSize.x), rand_range(0, screenSize.y))
-	
+		while $Player.position.distance_to(c.position) < 300:
+			c.position = Vector2(rand_range(0, screenSize.x), rand_range(0, screenSize.y))
 	pass
--- CoinDash/Player.gd
+++ CoinDash/Player.gd
@@ -6,6 +6,8 @@ export (float) var speed = 100.0;
 var velocity;
 
 func _ready():
+	set_process(false)
+	hide()
 	pass
 
 func _process(delta):
@@ -19,6 +21,20 @@ func _process(delta):
 	if Input.is_action_pressed("ui_down"):
 		velocity.y+=1
 	position += speed*velocity.normalized()*delta;
+	if position.x < $CollisionShape2D.shape.get_extents().x/2:
+		position.x = $CollisionShape2D.shape.get_extents().x/2
+	if position.y < $CollisionShape2D.shape.get_extents().y/2:
+		position.y = $CollisionShape2D.shape.get_extents().y/2
+	if position.x > get_viewport().get_visible_rect().size.x - $CollisionShape2D.shape.get_extents().x/2:
+		position.x = get_viewport().get_visible_rect().size.x -$CollisionShape2D.shape.get_extents().x/2
+	if position.y > get_viewport().get_visible_rect().size.y - $CollisionShape2D.shape.get_extents().y/2:
+		position.y = get_viewport().get_visible_rect().size.y -$CollisionShape2D.shape.get_extents().y/2
+
+	pass
+
+func start():
+	show()
+	set_process(true)
 	pass
 
 func die():
