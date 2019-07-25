53e2dab787a45d4cdba72123976afb972c96b8f9
TateKennington <TateKennington@users.noreply.github.com>
2019-07-25 16:15:21 +1200

    Added coin fadeout

--- CoinDash/Coin.gd
+++ CoinDash/Coin.gd
@@ -1,5 +1,16 @@
 extends Area2D
 
+func _ready():
+	$Tween.interpolate_property($AnimatedSprite, 'modulate',
+								Color(1,1,1,1), Color(1,1,1,0), 0.3,
+								Tween.TRANS_QUAD, Tween.EASE_OUT)
+	pass
+
 func pickup():
+	monitoring = false;
+	$Tween.start()
+	pass
+
+func _on_Tween_tween_completed(object, key):
 	queue_free()
 	pass
